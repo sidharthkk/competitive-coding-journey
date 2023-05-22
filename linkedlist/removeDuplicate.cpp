6c5e81ec793258865b2cfc7c4d48e6526754faa9
sidharthkk <96009053+sidharthkk@users.noreply.github.com>
2023-05-22 23:40:36 +0530

    Create removeDuplicate.cpp

---
+++ linkedlist/removeDuplicate.cpp
@@ -0,0 +1,34 @@
+/*struct Node {
+	int val;
+	struct Node* next;
+	Node(int x){
+		val = x;
+		next = NULL;
+	}
+};*/
+
+Node* rotateRight(Node* head, int R){
+    if(head==NULL ||head->next==NULL ||R==0)
+     return head;
+     
+     Node* current=head;
+     int len=1;
+     while(current->next!=NULL)
+     {
+         len++;
+         current=current->next;
+     }
+     current->next=head;
+    R=R%len;
+    R=len-R;
+    
+    while(R--)
+    {
+        current=current->next;
+        
+    }
+    head=current->next;
+    current->next=NULL;
+    
+    return head;
+}
